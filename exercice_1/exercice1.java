022e781f0319461b8d01a27f519ded7aeb936724
Teddy-Aimerick-TCHINDA-TAKUETE-3iL <tchindte@3il.fr>
2024-11-26 15:56:11 +0100

    modification du fichier exercice1 pour appliquer une meilleur qualite de code

--- exercice_1/exercice1.java
+++ exercice_1/exercice1.java
@@ -1,22 +1,69 @@
 public class FactureManager {
 
-    // Calcul le total d'une facture
+    /** 
+     * Fonction reductionAlimentaire qui applique une reduction sur le montant d'une facture de produit alimentaire;
+     * @param total : parametre correspondant au montant total de la facture sur laquel la reduction sera faite;
+     * @return : retour d'un double correspondant au montant avec reduction;
+    */
+    public double reductionAlimentaire(double total){
+        return total - total * 0.05; // Réduction de 5%
+    }
+
+    /** 
+     * Fonction reductionElectronique qui applique une reduction sur le montant d'une facture de produit electronique;
+     * @param total : parametre correspondant au montant total de la facture sur laquel la reduction sera faite;
+     * @return : retour d'un double correspondant au montant avec reduction;
+    */
+    public double reductionElectronique(double total){
+        return total - total * 0.1; // Réduction de 10%
+    }
+
+    /** 
+     * Fonction reductionLuxe qui applique une reduction sur le montant d'une facture de produit luxe;
+     * @param total : parametre correspondant au montant total de la facture sur laquel la reduction sera faite;
+     * @return : retour d'un double correspondant au montant avec reduction;
+    */
+    public double reductionLuxe(double total){
+        return total -= total * 0.15; // Réduction de 15%
+    }
+
+    /** 
+     * Fonction reductionFinal qui applique une reduction sur le montal final d'une facture;
+     * @param total : parametre correspondant au montant total de la facture sur laquel la reduction sera faite;
+     * @return : retour d'un double correspondant au montant avec reduction;
+    */
+    public double reductionFinal(double total){
+        if (total > 1000)
+            return total - total * 0.05; // Réduction supplémentaire de 5% pour les gros montants
+        return total;
+    }
+
+    /** 
+     * Fonction calculerFacture qui renvoit le montant final d'une facture apres y avoir appliquer les differentes reductions;
+     * @param typeProduit : parametre correspondant au type du produit en question;
+     * @param quantite : parametre correspondant a la quantite total de produit acheter;
+     * @param prixUnitaire : parametre correspondant au prix d'un article;
+     * @return : retour d'un double correspondant au montant final de la facture;
+    */
     public double calculerFacture(String typeProduit, int quantite, double prixUnitaire) {
         double total = quantite * prixUnitaire;
 
         // reduction selon la catégorie
-        if (typeProduit.equals("Alimentaire")) {
-            total -= total * 0.05; // Réduction de 5%
-        } else if (typeProduit.equals("Electronique")) {
-            total -= total * 0.1; // Réduction de 10%
-        } else if (typeProduit.equals("Luxe")) {
-            total -= total * 0.15; // Réduction de 15%
+        switch(typeProduit)
+        {
+            case "Alimentaire" :
+                                total = reductionAlimentaire(total);
+                                break;
+            case "Electronique" :
+                                total = reductionElectronique(total);
+                                break;
+            case "Luxe" : 
+                            total = reductionLuxe(total);
+                            break;
         }
 
         // Reduction sur le total
-        if (total > 1000) {
-            total -= total * 0.05; // Réduction supplémentaire de 5% pour les gros montants
-        }
+        total = reductionFinal(total);
 
         return total;
     }
